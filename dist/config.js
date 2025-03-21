09ac8ccbe9cd95b4194e55e8cf775d6e5ef5f624
Rodrigo Osorio <rodrigo.osorio.rl@gmail.com>
2025-03-21 11:51:57 -0600

    first commit

---
+++ dist/config.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DB_PORT = exports.DB_NAME = exports.DB_PASSWORD = exports.DB_USER = exports.DB_HOST = exports.PORT = void 0;
+exports.PORT = process.env.PORT || 3001;
+exports.DB_HOST = process.env.DB_HOST || "localhost";
+exports.DB_USER = process.env.DB_USER || "AdministradorSmec";
+exports.DB_PASSWORD = process.env.DB_PASSWORD || "Administrador";
+exports.DB_NAME = process.env.DB_NAME || "smec";
+exports.DB_PORT = process.env.DB_PORT || 3306;
