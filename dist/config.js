cf6aa9294f4ed114450dd7935c69c4910fc07aab
Rodrigo Osorio <rodrigo.osorio.rl@gmail.com>
2025-03-22 13:54:23 -0600

    agregamos el url de la abse de datos

--- dist/config.js
+++ dist/config.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DB_PORT = exports.DB_NAME = exports.DB_PASSWORD = exports.DB_USER = exports.DB_HOST = exports.PORT = void 0;
 exports.PORT = process.env.PORT || 3000;
-exports.DB_HOST = process.env.DB_HOST || 'localhost';
-exports.DB_USER = process.env.DB_USER || 'AdministradorSmec';
-exports.DB_PASSWORD = process.env.DB_PASSWORD || 'Administrador';
-exports.DB_NAME = process.env.DB_NAME || 'smec';
-exports.DB_PORT = Number(process.env.DB_PORT) || 3306;
+exports.DB_HOST = process.env.DB_HOST || 'shinkansen.proxy.rlwy.net';
+exports.DB_USER = process.env.DB_USER || 'root';
+exports.DB_PASSWORD = process.env.DB_PASSWORD || 'gMimNidewoBuTtAyDodFxEIRQMfhKFwr';
+exports.DB_NAME = process.env.DB_NAME || 'railway';
+exports.DB_PORT = Number(process.env.DB_PORT) || 25181;
--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.DB_HOST || 'localhost';
-export const DB_USER = process.env.DB_USER || 'AdministradorSmec';
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'Administrador';
-export const DB_NAME = process.env.DB_NAME || 'smec';
-export const DB_PORT = Number(process.env.DB_PORT) || 3306;
+export const DB_HOST = process.env.DB_HOST || 'shinkansen.proxy.rlwy.net';
+export const DB_USER = process.env.DB_USER || 'root';
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'gMimNidewoBuTtAyDodFxEIRQMfhKFwr';
+export const DB_NAME = process.env.DB_NAME || 'railway';
+export const DB_PORT = Number(process.env.DB_PORT) || 25181;
